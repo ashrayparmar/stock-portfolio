e3d24382cdb3199b9de9340a86dfbc9057adcd0e
Ashray Parmar <ashrayparmar0307@gmail.com>
2021-01-19 18:56:48 +0530

    style: bgColor & font

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 var stockName = document.querySelector("#name");
 var stockPrice = document.querySelector("#price");
 var stockQuantity = document.querySelector("#quantity");
+var currentPrice = document.querySelector("#currentPrice");
+var outputDiv = document.querySelector("#output");
+var btn = document.querySelector("#btnCalculate");
 
-var stock = stockName.value;
-var price = stockPrice.value;
-var quantity = stockQuantity.value;
-
-var currentPrice = 1020;
-
-if ( currentPrice > price) {
-    var profit = currentPrice - price;
-    console.log("Congrats you're in a profit of " + profit + " Rs")
+function calculate() {
+    if(stockPrice.value == '' || stockQuantity.value == '' || currentPrice.value == '') {
+        alert("Vlaues cannot be null")
+    }    
+    else if(stockName.value == ''){
+        alert("Please enter stock name")
+    }
+    else {
+        checkProfitLoss()
+    }
 }
-else if ( currentPrice < price) {
-    var loss = price - currentPrice;
-    console.log("Sorry you're in a loss of " + loss + " Rs")
+
+function checkProfitLoss() {
+    
+    var buyPrice = stockPrice.value;
+    var quantity = stockQuantity.value;
+    var curPrice = currentPrice.value;
+    var total = buyPrice*quantity;
+    var curValue = curPrice*quantity;
+    
+    if(total === curValue) {
+        outputDiv.innerHTML = "No profit and loss currently"        
+    }
+    else if(curValue> total) {
+        var profit = curValue - total
+        var profitPercent = Math.round((profit/total)*100);
+        outputDiv.innerHTML= (profitPercent+"% gain on Investment of "+ total +" Rs");
+    }
+    else {
+        alert("loss")
+    }
 }
+
+
+btn.addEventListener("click", calculate);
